80fb524175265cc328cb727dbea05109079c1baa
TedoBen <tedobenmikel@gmail.com>
2025-11-07 15:42:56 +0100

    Fix 7-multi_c.js: handle missing and negative occurrences correctly

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const x = parseInt(process.argv[2], 10);
 
-if (!x || x < 1) {
+if (!process.argv[2] || isNaN(x)) {
   console.log('Missing number of occurrences');
 } else {
   for (let i = 0; i < x; i++) {
